58d94eda2b800e8212cee44d5581cea10e9c4ed4
linkisf <lincoln.sf11@gmail.com>
2022-05-23 17:24:20 -0300

    subindo projeto pronto

---
+++ index.js
@@ -0,0 +1,19 @@
+const customExpress = require('./config/customExpress')
+const conexao = require('./controllers/infraestrutura/conexao')
+const Tabelas = require('./controllers/infraestrutura/tabelas')
+
+conexao.connect(erro => {
+    if(erro){
+        console.log(erro)
+    } else {
+        console.log('Conectado ao banco com sucesso')
+
+        Tabelas.init(conexao)
+        
+        const app = customExpress()
+        app.listen(3000, () => console.log('servidor rodando na porta 3000'))
+    }
+})
+
+
+
